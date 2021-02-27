cfa8ea6640baf346217dc644756e64e7f3a03426
Rafael Rey <reyarcenegui@gmail.com>
2021-02-27 18:12:43 +0100

    Fixed race conditions giving crashes

--- src/async_concatenator.cpp
+++ src/async_concatenator.cpp
@@ -35,70 +35,90 @@ public:
 
 private:
     void timerCallback(const ros::TimerEvent& event){
+
+
         final_cloud_.points.clear();
-        ROS_INFO("Nº clouds: [%d, %d, %d, %d]", cloud_1_vec_.size(),cloud_2_vec_.size(),cloud_3_vec_.size(),cloud_4_vec_.size() );
+        ROS_INFO("Number of clouds: [%d, %d, %d, %d]", cloud_1_vec_.size(),cloud_2_vec_.size(),cloud_3_vec_.size(),cloud_4_vec_.size() );
         insertPoints(cloud_1_vec_, final_cloud_);
         insertPoints(cloud_2_vec_, final_cloud_);
         insertPoints(cloud_3_vec_, final_cloud_);
         insertPoints(cloud_4_vec_, final_cloud_);
+        ROS_INFO("Final number of points: [%ld]",final_cloud_.points.size() );
         pcl_conversions::toPCL(ros::Time::now(), final_cloud_.header.stamp);
         final_cloud_.header.frame_id = target_frame_;
         full_cloud_pub_.publish(final_cloud_);
-        cloud_1_vec_.clear();
-        cloud_2_vec_.clear();
-        cloud_3_vec_.clear();
-        cloud_4_vec_.clear();
         
     }
     void insertPoints(std::vector<pcl::PointCloud<pcl::PointXYZI>> &in_cloud, pcl::PointCloud<pcl::PointXYZI> &result_cloud){
         
+        if(in_cloud.points.size() == 0)
+            return;
+
         for(auto &it: in_cloud){
             for(auto &point_it: it){
-                std::cout << "Point" << point_it.x << point_it.y << std::endl;
+                // std::cout << "Point" << point_it.x << point_it.y << std::endl;
                 result_cloud.points.push_back(point_it);
             }
         }
-        in_cloud.clear();
+        // in_cloud.clear();
     }
 
     void callback1(const sensor_msgs::PointCloud2::ConstPtr &msg)
     {
-        ROS_INFO("Callback 1");
+        // ROS_INFO("Callback 1");
         sensor_msgs::PointCloud2 msg_target_frame;
         pcl_ros::transformPointCloud(target_frame_, *msg, msg_target_frame, tf_listener_);
         pcl::PointCloud<pcl::PointXYZI> cloud;
         pcl::fromROSMsg(msg_target_frame, cloud);
         cloud_1_vec_.push_back(cloud);
+         
+         for(int i = 0; i < cloud_1_vec_.size(); ++i){
+            if(cloud_1_vec_[i].header.stamp - final_cloud_.header.stamp > 1e6 * time_interval_)
+                cloud_1_vec_.erase(cloud_1_vec_.begin()+i);
+        }
     }
     void callback2(const sensor_msgs::PointCloud2::ConstPtr &msg)
     {
-        ROS_INFO("Callback 2");
+        // ROS_INFO("Callback 2");
 
         sensor_msgs::PointCloud2 msg_target_frame;
         pcl_ros::transformPointCloud(target_frame_, *msg, msg_target_frame, tf_listener_);
         pcl::PointCloud<pcl::PointXYZI> cloud;
         pcl::fromROSMsg(msg_target_frame, cloud);
         cloud_2_vec_.push_back(cloud);
+        for(int i = 0; i < cloud_2_vec_.size(); ++i){
+            if(cloud_2_vec_[i].header.stamp - final_cloud_.header.stamp > 1e6 * time_interval_)
+                cloud_2_vec_.erase(cloud_2_vec_.begin()+i);
+        }
     }
     void callback3(const sensor_msgs::PointCloud2::ConstPtr &msg)
     {
-        ROS_INFO("Callback 3");
-
+        // ROS_INFO("Callback 3");
         sensor_msgs::PointCloud2 msg_target_frame;
         pcl_ros::transformPointCloud(target_frame_, *msg, msg_target_frame, tf_listener_);
         pcl::PointCloud<pcl::PointXYZI> cloud;
         pcl::fromROSMsg(msg_target_frame, cloud);
         cloud_3_vec_.push_back(cloud);
+
+         for(int i = 0; i < cloud_3_vec_.size(); ++i){
+            if(cloud_3_vec_[i].header.stamp - final_cloud_.header.stamp > 1e6 * time_interval_)
+                cloud_3_vec_.erase(cloud_3_vec_.begin()+i);
+        }
     }
     void callback4(const sensor_msgs::PointCloud2::ConstPtr &msg)
     {
-        ROS_INFO("Callback 4");
+        // ROS_INFO("Callback 4");
         
         sensor_msgs::PointCloud2 msg_target_frame;
         pcl_ros::transformPointCloud(target_frame_, *msg, msg_target_frame, tf_listener_);
         pcl::PointCloud<pcl::PointXYZI> cloud;
         pcl::fromROSMsg(msg_target_frame, cloud);
         cloud_4_vec_.push_back(cloud);
+
+        for(int i = 0; i < cloud_4_vec_.size(); ++i){
+            if(cloud_4_vec_[i].header.stamp - final_cloud_.header.stamp > 1e6 * time_interval_)
+                cloud_4_vec_.erase(cloud_4_vec_.begin()+i);
+        }
 
     }
 
